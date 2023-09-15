483d47e7c9b810ddbd5c2c9d3f04a2ee1bff1349
lr661208 <1612548376@qq.com>
2023-09-15 11:22:38 +0800

    提交测试3

--- src/main/java/com/qs/d117/a.java
+++ src/main/java/com/qs/d117/a.java
@@ -2,6 +2,6 @@ package com.qs.d117;
 
 public class a {
     public static void main(String[] args) {
-        System.out.println("123444446666");
+        System.out.println("12344555444");
     }
 }
