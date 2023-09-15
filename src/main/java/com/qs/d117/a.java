3fed042b3a8524b2dee41e5f952ae42d634a03bd
lr661208 <1612548376@qq.com>
2023-09-15 11:34:50 +0800

    提交测试4

--- src/main/java/com/qs/d117/a.java
+++ src/main/java/com/qs/d117/a.java
@@ -2,6 +2,6 @@ package com.qs.d117;
 
 public class a {
     public static void main(String[] args) {
-        System.out.println("11111231231");
+        System.out.println("11111231231我是你爹");
     }
 }
